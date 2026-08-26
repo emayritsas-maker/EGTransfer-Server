03ffb156e0a54e00236471406117c19e05ce5de4
emayritsas-maker <emayritsas@gmail.com>
2026-08-26 03:58:32 +0300

    Fix import path for database module in register.js

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../database/db");
+const db = require('../../database/db');
 const generateFriendCode = require("../utils/generateFriendCode");
 const { hashPassword } = require("../utils/hashPassword");
 
