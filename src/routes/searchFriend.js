1cbee2ef33e99b198abab462ebd81c3a8d7c3303
emayritsas-maker <emayritsas@gmail.com>
2026-08-28 20:45:13 +0300

    Refactor searchFriend route to use relative path

--- src/routes/searchFriend.js
+++ src/routes/searchFriend.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../../database/db');
+const db = require('../database/db');
 
 router.post("/", (req, res) => {
     const { friendcode } = req.body;
