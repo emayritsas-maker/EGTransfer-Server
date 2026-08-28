fa0062491ca7df1ae4507c4197e01ff7d8d38990
emayritsas-maker <emayritsas@gmail.com>
2026-08-28 20:37:46 +0300

    Fix route paths in server.js

--- src/server.js
+++ src/server.js
@@ -7,19 +7,18 @@ require("./database/db");
 
 app.use(express.json());
 
-// ROUTES
-app.use("/register", require("./src/routes/register"));
-app.use("/login", require("./src/routes/login"));
-app.use("/getMetadata", require("./src/routes/getMetadata"));
-app.use("/updateMetadata", require("./src/routes/updateMetadata"));
-app.use("/getFriendcode", require("./src/routes/getFriendcode"));
-app.use("/searchFriend", require("./src/routes/searchFriend"));
-app.use("/sendFriendRequest", require("./src/routes/sendFriendRequest"));
-app.use("/getFriendRequests", require("./src/routes/getFriendRequests"));
-app.use("/acceptFriendRequest", require("./src/routes/acceptFriendRequest"));
-app.use("/getFriends", require("./src/routes/getFriends"));
-app.use("/emailVerifyIP", require("./src/routes/emailVerifyIP"));
-app.use("/signal", require("./src/routes/signal"));
+app.use("/register", require("./routes/register"));
+app.use("/login", require("./routes/login"));
+app.use("/getMetadata", require("./routes/getMetadata"));
+app.use("/updateMetadata", require("./routes/updateMetadata"));
+app.use("/getFriendcode", require("./routes/getFriendcode"));
+app.use("/searchFriend", require("./routes/searchFriend"));
+app.use("/sendFriendRequest", require("./routes/sendFriendRequest"));
+app.use("/getFriendRequests", require("./routes/getFriendRequests"));
+app.use("/acceptFriendRequest", require("./routes/acceptFriendRequest"));
+app.use("/getFriends", require("./routes/getFriends"));
+app.use("/emailVerifyIP", require("./routes/emailVerifyIP"));
+app.use("/signal", require("./routes/signal"));
 
 app.listen(3000, () => {
     console.log("EGTransfer Server running on port 3000");
