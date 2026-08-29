84f8f6bdb09ab94b33d44a7db2c2690df5a7c1d5
emayritsas-maker <emayritsas@gmail.com>
2026-08-29 04:14:04 +0300

    Update schema.sql

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -9,3 +9,27 @@ CREATE TABLE IF NOT EXISTS users (
     isVerified INTEGER,
     verificationCode TEXT
 );
+
+CREATE TABLE IF NOT EXISTS metadata (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER,
+    key TEXT,
+    value TEXT,
+    FOREIGN KEY(userId) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER,
+    friendId INTEGER,
+    FOREIGN KEY(userId) REFERENCES users(id),
+    FOREIGN KEY(friendId) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS blocked (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER,
+    blockedId INTEGER,
+    FOREIGN KEY(userId) REFERENCES users(id),
+    FOREIGN KEY(blockedId) REFERENCES users(id)
+);
